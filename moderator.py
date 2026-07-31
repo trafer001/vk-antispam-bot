ce3fc07e433fd77f7e8c516414f0de58d989eb73
trafer001 <mbdhmjzn@gmail.com>
2026-07-31 20:45:33 +0300

    Update moderator.py

--- moderator.py
+++ moderator.py
@@ -1,1 +1,16 @@
- 
+def is_admin(user_id, group):
+    return user_id in group.get("admins", [])
+
+
+def is_trusted(user_id, group):
+    return user_id in group.get("trusted_users", [])
+
+
+def should_delete(user_id, group):
+    if is_admin(user_id, group):
+        return False
+
+    if is_trusted(user_id, group):
+        return False
+
+    return True
