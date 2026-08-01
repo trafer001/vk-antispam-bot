88b3910767fb0563f0007b9f0817ef83c896a0c5
trafer001 <mbdhmjzn@gmail.com>
2026-08-01 10:36:21 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,18 @@
- 
+from datetime import datetime
+
+
+def log(message):
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    text = f"[{now}] {message}"
+
+    print(text)
+
+    with open(
+        "bot.log",
+        "a",
+        encoding="utf-8"
+    ) as file:
+
+        file.write(text + "\n")
